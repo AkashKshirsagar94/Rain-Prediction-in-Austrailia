8287517240a01b414a1585d38b153f1ed3da75b4
[Akash Kshirsagar] <[akky.kshirsagar@gmail.com]>
2024-10-11 13:32:42 +0530

    Set up exception & logging file

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys
 
-def error_message_detail(error):
+def error_message_detail(error, error_detail:sys):
     """
     Generates a detailed error message, including file name, line number, and error message.
 
@@ -20,3 +20,10 @@ def error_message_detail(error):
     error_message = f"Error occurred in Python script '{file_name}' on line {line_number}: {str(error)}"
 
     return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message, error_detail)
+    def __str__(self):
+        return self.error_message
